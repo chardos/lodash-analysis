3043294a5c3ce2ab5f39d7223bac1ba16eb307e0
Richard Tan <chardos@gmail.com>
2019-03-18 23:12:25 +1100

    Add webpack-bundle-analyzer

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
+};
