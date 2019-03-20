779358508ce1d41a85f1618abc7108fb69b80d8a
Richard Tan <chardos@gmail.com>
2019-03-20 21:49:32 +1100

    lodash analysis

--- src/index.js
+++ src/index.js
@@ -1,1 +1,86 @@
-console.log('hey')
+import drop from 'lodash/drop';
+import omit from 'lodash/omit';
+import fill from 'lodash/fill';
+import flatten from 'lodash/flatten';
+import head from 'lodash/head';
+import zip from 'lodash/zip';
+import debounce from 'lodash/debounce';
+import conforms from 'lodash/conforms';
+import flow from 'lodash/flow';
+import escape from 'lodash/escape';
+// import countBy from 'lodash/countBy';
+// import filter from 'lodash/filter';
+// import now from 'lodash/now';
+// import curry from 'lodash/curry';
+// import camelCase from 'lodash/camelCase';
+// import union from 'lodash/union';
+// import kebabCase from 'lodash/kebabCase';
+// import stubString from 'lodash/stubString';
+// import replace from 'lodash/replace';
+// import findLastKey from 'lodash/findLastKey';
+
+// import drop from 'lodash-es/drop';
+// import omit from 'lodash-es/omit';
+// import fill from 'lodash-es/fill';
+// import flatten from 'lodash-es/flatten';
+// import head from 'lodash-es/head';
+// import zip from 'lodash-es/zip';
+// import debounce from 'lodash-es/debounce';
+// import conforms from 'lodash-es/conforms';
+// import flow from 'lodash-es/flow';
+// import escape from 'lodash-es/escape';
+// import countBy from 'lodash-es/countBy';
+// import filter from 'lodash-es/filter';
+// import now from 'lodash-es/now';
+// import curry from 'lodash-es/curry';
+// import camelCase from 'lodash-es/camelCase';
+// import union from 'lodash-es/union';
+// import kebabCase from 'lodash-es/kebabCase';
+// import stubString from 'lodash-es/stubString';
+// import replace from 'lodash-es/replace';
+// import findLastKey from 'lodash-es/findLastKey';
+
+// import drop from 'lodash.drop';
+// import omit from 'lodash.omit';
+// import fill from 'lodash.fill';
+// import flatten from 'lodash.flatten';
+// import head from 'lodash.head';
+// import zip from 'lodash.zip';
+// import debounce from 'lodash.debounce';
+// import conforms from 'lodash.conforms';
+// import flow from 'lodash.flow';
+// import escape from 'lodash.escape';
+// import countBy from 'lodash.countBy';
+// import filter from 'lodash.filter';
+// import now from 'lodash.now';
+// import curry from 'lodash.curry';
+// import camelCase from 'lodash.camelCase';
+// import union from 'lodash.union';
+// import kebabCase from 'lodash.kebabCase';
+// import stubString from 'lodash.stubString';
+// import replace from 'lodash.replace';
+// import findLastKey from 'lodash.findLastKey';
+ 
+drop
+omit
+fill
+flatten
+head
+
+zip
+debounce
+conforms
+flow
+escape
+
+// countBy
+// filter
+// now
+// curry
+// camelCase
+
+// union
+// kebabCase
+// stubString
+// replace
+// findLastKey
